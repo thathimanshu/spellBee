7f654a19ec8f0ecc27b403c3908511f1d4b86a1e
thathimanshu <thathimanshusaini@gmail.com>
2024-09-27 07:31:30 +0530

    btns working

--- Frontend/wordGame/src/Letter.jsx
+++ Frontend/wordGame/src/Letter.jsx
@@ -1,6 +1,15 @@
+import { useContext ,useEffect } from 'react';
+import { AppContext } from './AppContext';
+
 export default function Letter({ bgColor, text, className = "" }) {
+  const {setPValue } = useContext(AppContext);
+
+  const btnClick = (ch) => {
+    setPValue((prev) => prev + ch);
+  };
+  
   return (
-    <div className={`relative mx-auto w-[100px] h-[55px] hexagon m-4 ml-1 mr-1 ${className} cursor-pointer`}>
+    <div onClick={() => btnClick(text)}  className={`relative mx-auto w-[100px] h-[55px] hexagon m-4 ml-1 mr-1 ${className} cursor-pointer`}>
       <div
         className="flex items-center justify-center w-[100px] h-[55px]"
         style={{ backgroundColor: bgColor }}  // Using inline style for dynamic bgColor
