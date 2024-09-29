364d3b4ea58215a00e004ad545c05645ea7dc215
thathimanshu <thathimanshusaini@gmail.com>
2024-09-29 10:26:55 +0530

    respo

--- Frontend/wordGame/src/Dash.jsx
+++ Frontend/wordGame/src/Dash.jsx
@@ -1,6 +1,6 @@
 function Dash(){
     return(
-        <div className="h-0.5 sm:h-1 sm:w-7 w-5 bg-themeColor dark:bg-themeColorDark"></div>
+        <div className="h-0.5 sm:h-1 sm:w-7 w-4 bg-themeColor dark:bg-themeColorDark"></div>
     )
 }
 export default Dash
